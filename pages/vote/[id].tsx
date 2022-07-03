2c6551866bf24f6ad7416b45cd461b7b7144587a
bradleygichuru <prz03@protonmail.com>
2022-07-03 19:32:37 +0300

    feat([id].tsx): add voting functionality

--- pages/vote/[id].tsx
+++ pages/vote/[id].tsx
@@ -1,35 +1,76 @@
+import { SourceMap } from "module";
 import type { GetServerSideProps, NextPage } from "next";
-import { useState } from "react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 import { trpc } from "../../lib/trpc";
+import puff from "../../public/puff.svg";
 //TODO handle votes
+
 const Vote: NextPage = (props: any) => {
-  const [vote, setVote] = useState("");
+  const [voted, setVoted] = useState(false);
+  const [loading,setLoading] = useState(false);
+  
   console.log(props.id);
+  useEffect(()=>{
+    console.log("voted",voted)
+  },[voted]);
+  const getPoll = () =>{}
   const { data, error, isLoading } = trpc.useQuery([
     "get-poll",
     { id: props.id as string },
   ]);
+  
+  const castvoteMutation = trpc.useMutation(["cast-vote"]);
+  const castVote = (id: string, e: any) => {
+    /* e.preventDefault(); */
+    castvoteMutation.mutate({ opinionId: id });
+    setVoted(voted=>!voted);
+  }
   console.log(data);
+  if (isLoading == true) {
+    return (
+      <div className="grid h-screen place-items-center">
+        <Image
+          src={puff}
+          width={64}
+          height={64}
+          alt="loading..."
+          className=""
+        />
+      </div>
+    );
+  }
   return (
     <div className="grid h-screen place-items-center">
-      <p className="text-2xl font-titan">Pollage</p>
+      <p className="text-2xl font-titan">{data?.name}</p>
+      <p>{`${voted}`}</p>
       <div className="flex ">
         <div className="flex flex-col m-20 ">
-          <div className="bg-[#F5F5F5] justify-center flex rounded-xl border-2 border-[#808080] h-28 max-w-xs">
-            afwawf
+          <button
+            className="bg-[#F5F5F5] justify-center flex rounded-xl border-2 border-[#808080] h-28 max-w-xs btn btn-outline btn-primary"
+            onClick={(e) => {
+              castVote(data!.opinions[0]!.id, e);
+            }}
+          >
+            {`Vote ${data?.opinions[0].description}`}
+          </button>
+          <div className="flex justify-center">
+            <p className="mt-6">{data?.opinions[0].count}</p>
           </div>
-          <p className="mt-5 font-extrabold	text-2xl font-jetbrains">
-            {data?.options[0].description}
-          </p>
         </div>
         <p className="m-20 font-bold">vs</p>
         <div className="flex flex-col m-20 ">
-          <div className="bg-[#F5F5F5] justify-center flex border-2 rounded-xl border-[#808080] h-28 max-w-xs">
-            afwawf
+          <button
+            className="bg-[#F5F5F5] justify-center flex border-2 rounded-xl border-[#808080] h-28 max-w-xs btn btn-outline btn-primary"
+            onClick={(e) => {
+              castVote(data!.opinions[1]!.id, e);
+            }}
+          >
+            {`Vote ${data?.opinions[1].description}`}
+          </button>
+          <div className="flex justify-center">
+            <p className="mt-6">{data?.opinions[1].count}</p>
           </div>
-          <p className="mt-5 font-extrabold	text-2xl font-jetbrains">
-            {data?.options[1].description}
-          </p>
         </div>
       </div>
     </div>
