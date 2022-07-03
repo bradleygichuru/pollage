e096d9ffca900c08f452c5a730b4d67fb4036433
bradleygichuru <prz03@protonmail.com>
2022-07-03 19:29:41 +0300

    feat(trpc.ts): adds cast-vote mutation

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -1,39 +1,62 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { z } from "zod";
-import prisma from "../../../lib/prisma";
 
-export const appRouter = trpc.router().mutation("poll-add", {
-  input: z
-    .object({
-      opinion1: z.string(),
-      opinion2: z.string(),
-      pollDescription: z.string(),
-    })
-    .nullish(),
- async resolve({ input }) {
-    console.log(input); //TODO remove debug logs
-    let res = await prisma.poll.create({
-      data: {
-        name: input!.pollDescription,
-        options: { createMany: { data: [{ description: input!.opinion1,count:0 },{description:input!.opinion2,count:0}] } },
-      },
-    });
-    console.log(res)
-    return {
-      res,
-    };
-  },
-}).query("get-poll",{
-  input:z
-  .object({
-    id: z.string()
-  }).nullish(),
-  async resolve({input}){
-    let res = await prisma.poll.findUnique({where:{id:input?.id},include:{options:true}})
-    return res;
-  }
-});
+import { z } from "zod";
+import { prisma } from "../../../lib/prisma";
+
+export const appRouter = trpc
+  .router()
+  .mutation("poll-add", {
+    input: z
+      .object({
+        opinion1: z.string(),
+        opinion2: z.string(),
+        pollDescription: z.string(),
+      })
+      .nullish(),
+    async resolve({ input }) {
+      console.log(input); //TODO remove debug logs
+      let res = await prisma.poll.create({
+        data: {
+          name: input?.pollDescription!,
+          opinions: {
+            createMany: {
+              data: [
+                { description: input?.opinion1!, count: 0 },
+                { description: input?.opinion2!, count: 0 },
+              ],
+            },
+          },
+        },
+      });
+      console.log(res);
+      return {
+        res,
+      };
+    },
+  })
+  .mutation("cast-vote", {
+    input: z.object({ opinionId: z.string() }).nullish(),
+    async resolve({ input }) {
+      console.log(input);
+      let result = await prisma.opinion.update({where:{id:input?.opinionId},data:{count:{increment:1}}})
+      return result;
+    },
+  })
+  .query("get-poll", {
+    input: z
+      .object({
+        id: z.string(),
+      })
+      .nullish(),
+    async resolve({ input }) {
+      let res = await prisma.poll.findUnique({
+        where: { id: input?.id },
+        include: { opinions: true },
+      });
+      return res;
+    },
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
